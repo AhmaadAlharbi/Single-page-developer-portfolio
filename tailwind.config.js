35b5bc63050ddbd8fb99c986ec31b494d750d4fa
ahmadZaid <ahmaadzaid7@gmail.com>
2022-11-12 00:30:45 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        Black: "#151515",
+        DarkGrey: "#242424",
+        Green: "#4EE1A0",
+        Grey: "#D9D9D9",
+        White: "#FFFFFF",
+      },
+    },
+  },
+  plugins: [],
+};
